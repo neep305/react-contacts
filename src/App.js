fb47f9f9d6cf7c4cd7c291be769c231d4ed2e6e3
neep305 <neep305@gmail.com>
2017-12-29 09:14:49 +0900

    add src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import IngredientList from './IngredientList';
+import CreateContact from './CreateContact';
+
+import ContactsAPI from './utils/ContactsAPI';
 
 import logo from './logo.svg';
 import './App.css';
@@ -30,32 +33,20 @@ const ingredient = {"items":[{"id":"a"},{"id":"b"},{"id":"c"}]};
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    screen:'create',
+    contacts:[]
+  }
+
+  componentDidMount(){
+
   }
 
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+
+    ContactsAPI.remove(contact)
   }
 
   render() {
@@ -70,7 +61,7 @@ class App extends Component {
         </p> */}
 
         <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
-        <IngredientList items={ingredient.items} />
+        <CreateContact />
       </div>
     );
   }
